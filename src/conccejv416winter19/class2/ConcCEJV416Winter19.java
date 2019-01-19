8b11838c36c6dc11cf779762af10456cd7a20cf7
R_IDD <R_IDD@CCE-FB107-01W.concordia.ca>
2019-01-18 19:33:50 -0500

    Initial commit

---
+++ src/conccejv416winter19/class2/ConcCEJV416Winter19.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conccejv416winter19.class2;
+
+/**
+ *
+ * @author R_IDD
+ */
+public class ConcCEJV416Winter19 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
